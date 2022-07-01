42f55490c7b98d79fac8c643222f572c26444693
WEBALT-g <contact@webalt-g.com>
2022-07-01 17:39:30 +0200

    fin cours formulaires

--- functions.php
+++ functions.php
@@ -18,5 +18,29 @@ function nav_menu (string $linkClass = ''): string
     return
     nav_item('index.php', 'Accueil', $linkClass) .
     nav_item('contact.php', 'Contact', $linkClass);
+    nav_item('jeu.php', 'Jeu', $linkClass);
 }
+
+function checkbox (string $name, string $value, array $data): string
+{
+    $attributes = '';
+    if (isset($data[$name]) && in_array($value, $data[$name])) {
+        $attributes .= 'checked';
+    }
+    return <<<HTML
+    <input type="checkbox" name="{$name}[]" value="$value" $attributes>
+HTML;
+}
+
+function radio (string $name, string $value, array $data): string
+{
+    $attributes = '';
+    if (isset($data[$name]) && $value === $data[$name]) {
+        $attributes .= 'checked';
+    }
+    return <<<HTML
+    <input type="radio" name="{$name}" value="$value" $attributes>
+HTML;
+}
+
 ?>
--- menu.php
+++ menu.php
@@ -1,2 +1,3 @@
 <?= nav_item('index.php', 'Accueil', $class); ?>
 <?= nav_item('contact.php', 'Contact', $class); ?>
+<?= nav_item('jeu.php', 'Jeu', $class); ?>
